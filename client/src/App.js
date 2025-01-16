691ce3bc6e05c17e6aec5e18a601a31766c61d8c
mustafa durmus <mdurmusnl@gmail.com>
2025-01-16 20:20:59 +0100

    Fix: Add client folder as a regular directory

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+
+import GymMedia from './components/GymMedia';
+import React from 'react'
+import HomePage from './pages/HomePage'
+function App() {
+  return (
+    <div className="App">
+    
+    hello world
+    <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+
+import React from 'react';  
+import GymMedia from '../components/GymMedia';
+
+const HomePage = () => {
+    return (
+        <div>
+        <h1>Home Page</h1>
+        <GymMedia />
+        </div>
+    );
+    }
+    export default HomePage;
