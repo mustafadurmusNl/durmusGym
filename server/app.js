e41fe042e7d68b060160bad303fdea8eb64fa2ec
mustafa durmus <mdurmusnl@gmail.com>
2025-01-20 13:40:57 +0100

    Fix: Add server/app.js for route handling

--- server/app.js
+++ server/app.js
@@ -3,17 +3,18 @@ const cors = require('cors');
 const mediaRoutes = require('./routes/mediaRoutes');
 
 const app = express();
-console.log('Pexels API Key:', process.env.PEXELS_API_KEY ? 'Loaded' : 'Not Loaded');
-console.log('Pexels API Key:', process.env.PEXELS_API_KEY);
-console.log(process.env);  // This will print all environment variables
 
 // Middleware
-// Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL, // Use FRONTEND_URL from the environment variables
-    credentials: true, // If you're using cookies or other credentials
-  }));
+  origin: process.env.FRONTEND_URL, // Use FRONTEND_URL from the environment variables
+  credentials: true, // If you're using cookies or other credentials
+}));
 app.use(express.json());
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
 
 // Routes
 app.use('/api', mediaRoutes);
