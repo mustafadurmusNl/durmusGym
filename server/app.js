a0b34456da3465d55d52f969e8b4e78783fcb70a
mustafa durmus <mdurmusnl@gmail.com>
2025-01-20 10:32:06 +0100

    Fix Heroku deployment setup

--- server/app.js
+++ server/app.js
@@ -8,7 +8,11 @@ console.log('Pexels API Key:', process.env.PEXELS_API_KEY);
 console.log(process.env);  // This will print all environment variables
 
 // Middleware
-app.use(cors());
+// Middleware
+app.use(cors({
+    origin: process.env.FRONTEND_URL, // Use FRONTEND_URL from the environment variables
+    credentials: true, // If you're using cookies or other credentials
+  }));
 app.use(express.json());
 
 // Routes
