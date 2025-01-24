99f4bbbcf76f659c2419a50e6309a4880f1c7ad1
mustafa durmus <mdurmusnl@gmail.com>
2025-01-24 20:27:53 +0100

    About page is implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import NavBar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
-import PersonalTraining from "./components/PersonalTraining"; // Import the PersonalTraining component
-import Pilates from "./components/Pilates"; // Import the Pilates component
-import Diet from "./components/Diet"; // Import the Diet component
+import PersonalTraining from "./components/PersonalTraining"; 
+import Diet from "./components/Diet"; 
+import About from "./components/About";
+import Pilates from "./components/Pilates";
 const App = () => {
   return (
     <Router>
@@ -12,9 +14,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/personal-training" element={<PersonalTraining />} />
-        <Route path="/pilates" element={<Pilates />} /> {/* Use Pilates component here */}
-        <Route path="/diet" element={<Diet />} /> {/* Use Diet component here */}
-        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/pilates" element={<Pilates />} /> 
+        <Route path="/diet" element={<Diet />} /> 
+        <Route path="/about" element={<About />} /> 
         <Route path="/method" element={<h1>Method</h1>} />
         <Route path="/contact" element={<h1>Contact</h1>} />
         <Route path="/free-trial" element={<h1>Free Trial Lesson</h1>} />
