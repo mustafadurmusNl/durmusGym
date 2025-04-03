b3a25c7bd03c3183e625bd7f2fd6f2f6606a28fb
mustafa durmus <mdurmusnl@gmail.com>
2025-04-03 14:54:50 +0200

    google map was added in Contact page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -29,7 +29,7 @@ const Contact = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
       const response = await fetch(`${BASE_URL}/api/messages/contact`, {
         method: "POST",
@@ -54,6 +54,7 @@ const Contact = () => {
 
   return (
     <div className="about-container">
+      {/* Left Section - Contact Form */}
       <div className="about-left">
         <h2>{translations.title || "Schedule your free trial lesson"}</h2>
         <p>{translations.subtitle || "After completing the form below, we will contact you."}</p>
@@ -85,6 +86,21 @@ const Contact = () => {
         </form>
 
         {responseMessage && <p className="response-message">{responseMessage}</p>}
+      </div>
+
+      {/* Right Section - Google Map */}
+      <div className="about-right">
+        <div className="map-container">
+          <iframe
+            title="Durmus Gym Location"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2462.875185905202!2d4.277808476015456!3d51.4954162178691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c44259ec2f43cf%3A0x3089d22cfa8d31e9!2sMeeroeversstraat%2056%2C%204611%20Bergen%20op%20Zoom%2C%20Netherlands!5e0!3m2!1sen!2snl!4v1614975463246!5m2!1sen!2snl"
+            width="100%"
+            height="500"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
