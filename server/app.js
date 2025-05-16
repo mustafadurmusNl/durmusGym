031c3ba391e25bd4e17946824a22a0bde4e81caf
mustafa durmus <mdurmusnl@gmail.com>
2025-05-16 20:21:00 +0200

    back end serves not found page and ready for deployment

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const { fetchAndStoreImages, getImagesFromDB, isImageDataReady } = require("./services/imageService");
+const {
+  fetchAndStoreImages,
+  getImagesFromDB,
+  isImageDataReady,
+} = require("./services/imageService");
 
 const mediaRoutes = require("./routes/mediaRoutes");
 const languageRoutes = require("./routes/languageRoutes");
@@ -23,24 +27,31 @@ app.use("/api", mediaRoutes);
 app.use("/api/languages", languageRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Serve the static files from React's build folder
-app.use(express.static(path.join(__dirname, '../../../client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// Always return the main index.html for any other routes (client-side routing)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../client/build', 'index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
+
 
 // ✅ Dev/debug endpoint: Returns all images from the database
 app.get("/api/images/all", async (req, res) => {
   if (!isImageDataReady()) {
-    return res.status(503).json({ message: "Images are still being fetched, please try again later." });
+    return res
+      .status(503)
+      .json({
+        message: "Images are still being fetched, please try again later.",
+      });
   }
 
   try {
     const images = await getImagesFromDB();
     if (!images || images.length === 0) {
-      return res.status(404).json({ message: "No images found in the database." });
+      return res
+        .status(404)
+        .json({ message: "No images found in the database." });
     }
     res.status(200).json(images);
   } catch (error) {
