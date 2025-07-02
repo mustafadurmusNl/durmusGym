3416acc9895bb3473f573917997768654fb5e655
mustafa durmus <mdurmusnl@gmail.com>
2025-07-02 20:55:06 +0200

    profile page is build with its translation

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,12 +4,14 @@ import { Link, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { LanguageContext } from "../context/languageContext";
 import LanguageSelector from "./LanguageSelector";
+import { AuthContext } from "../context/AuthContext";
 import "../styles/Navbar.css";
 
 const NavBar = () => {
   const { t } = useTranslation();
   const { translations } = useContext(LanguageContext);
   const location = useLocation();
+  const { user, logout } = useContext(AuthContext);
 
   const translate = (key) => translations[key] || t(key);
   const isActive = (path) => (location.pathname === path ? "active" : "");
@@ -38,9 +40,8 @@ const NavBar = () => {
           <Link to="/diet">{translate("navbar.diet")}</Link>
         </li>
         <li className={isActive("/library")}>
-          <Link to="/library">{translate("navbar.library") || "Library"}</Link>
+          <Link to="/library">{translate("navbar.library")}</Link>
         </li>
-
         <li className={isActive("/about")}>
           <Link to="/about">{translate("navbar.about")}</Link>
         </li>
@@ -53,12 +54,39 @@ const NavBar = () => {
         <li className={isActive("/yoga")}>
           <Link to="/yoga">{translate("navbar.yoga")}</Link>
         </li>
-        <li className={isActive("/free-trial") + " cta"}>
-          <Link to="/free-trial-page">{translate("navbar.freeTrial")}</Link>
-        </li>
-        <li className={isActive("/login")}>
-          <Link to="/login">{translate("navbar.login")}</Link>
-        </li>
+
+        {/* User is logged in */}
+        {user ? (
+          <li className="navbar-user-dropdown">
+            <span className="welcome-message">
+              {translate("navbar.welcome")},{" "}
+              {user.firstName ||
+                user.name?.split(" ")[0] ||
+                user.email ||
+                "User"}
+              !
+            </span>
+            <ul className="dropdown-menu">
+              <li className={isActive("/profile")}>
+                <Link to="/profile">{translate("navbar.profile")}</Link>
+              </li>
+              <li>
+                <button className="logout-button" onClick={logout}>
+                  {translate("navbar.logout")}
+                </button>
+              </li>
+            </ul>
+          </li>
+        ) : (
+          <>
+            <li className={isActive("/login")}>
+              <Link to="/login">{translate("navbar.login")}</Link>
+            </li>
+            <li className={isActive("/free-trial-page") + " cta"}>
+              <Link to="/free-trial-page">{translate("navbar.freeTrial")}</Link>
+            </li>
+          </>
+        )}
 
         <li className="language-switcher">
           <LanguageSelector />
