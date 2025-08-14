64223aceb3cc403e37b6826e00d528a1e61a0a71
mustafa durmus <mdurmusnl@gmail.com>
2025-08-14 20:58:34 +0200

    refactor backend video protection

--- server/routes/mediaRoutes.js
+++ server/routes/mediaRoutes.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const { getImages, getVideos } = require("../controllers/mediaController");
-const authenticate = require("../middleware/authMiddleware");
-
 const router = express.Router();
 
 // ✅ Artık sadece giriş yapanlar erişebilir
-router.get("/images", authenticate, getImages);
-router.get("/videos", authenticate, getVideos);
+router.get("/images", getImages);
+router.get("/videos", getVideos); // protected
 
 module.exports = router;
